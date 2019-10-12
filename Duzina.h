e82d7fcc07418dbc833eb0615fb595efe6ff9432
MikiL023 <56232005+MikiL023@users.noreply.github.com>
2019-10-12 22:10:01 +0200

    Add files via upload

---
+++ Duzina.h
@@ -0,0 +1,17 @@
+#ifndef DUZINA_H
+#define DUZINA_H
+
+
+class Duzina
+{
+    public:
+         double m;
+         double sum;
+         Metri();
+         Milimetri();
+
+
+
+};
+
+#endif // DUZINA_H
